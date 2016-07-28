bd5c4a3d1f1fdc0eb8c31de3c92f2c5e916cad60
sanya-club <sanya-club@yandex.ru>
2016-07-28 19:36:59 +0300

    update script-2

--- js/script-2.js
+++ js/script-2.js
@@ -1,23 +1,16 @@
 var arr = [];
 for (var i = 0; i <= 4; i++) {
-
   arr[i] = prompt('введите имя');
-}
+};
 console.log(arr);
-
 var userName = prompt('введите имя пользователя');
-
 for (var i = 0; i <= arr.length; i++) {
   if (userName === arr[i]) {
-
     alert(arr[i] + ' ' + ', вы успешно вошли');
     break;
-  }
-
+  };
 };
-
 console.log('your name is', userName);
 if (userName != arr[i]) {
   alert('ERROR');
-
 };
