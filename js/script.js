991e49be48db929e10217cd7c176746283eba2b6
sanya-club <sanya-club@yandex.ru>
2016-07-29 10:08:36 +0300

    update script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-function pow() {
+function pow(number, power) {
   var number = prompt('введите число');
   var power = prompt('введите степень');
-  var result = Math.pow(number, power);
+  if (power == 0) {
+    var result = 1;
+  } else if (power < 0) {
+    result = number;
+    power = power * (-1);
+    for (i = 0; i < power - 1; i++) {
+      result = result * number;
+    }
+    result = 1 / result
+  } else {
+    result = number;
+    for (i = 0; i < power - 1; i++) {
+      result = result * number;
+    }
+  }
   return console.log('Ваш результат =', result);
 }
-
 pow();
